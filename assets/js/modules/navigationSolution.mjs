5a9277035ddc238734d3ccbc527ec0a2c2abdd4f
Juan Basgall <juanangelbasgall@hotmail.com>
2024-03-28 19:53:09 -0300

    Agregada paleta de colores

--- assets/js/modules/navigationSolution.mjs
+++ assets/js/modules/navigationSolution.mjs
@@ -1,20 +1,22 @@
-setTimeout(()=>{
-
-    if (document.location.hostname === 'd3vh4n5.github.io'){
-        console.warn('Creando redirección')
-
-        const basePath = 'https://d3vh4n5.github.io/CaC-Fullstack-Node-Grupo10'
-        const links = document.querySelectorAll('.INSIDE-NAV')
+document.addEventListener('DOMContentLoaded', ()=> {
+    setTimeout(()=>{
     
-        links.forEach( link => {
-            const navHref = link.attributes.href.value
-
-            link.addEventListener('click', (e)=> {
-                e.preventDefault()
-                const newPath = basePath + navHref
-                document.location.href = newPath
+        if (document.location.hostname === 'd3vh4n5.github.io'){
+            console.warn('Creando redirección')
+    
+            const basePath = 'https://d3vh4n5.github.io/CaC-Fullstack-Node-Grupo10'
+            const links = document.querySelectorAll('.INSIDE-NAV')
+        
+            links.forEach( link => {
+                const navHref = link.attributes.href.value
+    
+                link.addEventListener('click', (e)=> {
+                    e.preventDefault()
+                    const newPath = basePath + navHref
+                    document.location.href = newPath
+                })
             })
-        })
-    }
-
-}, 500)
+        }
+    
+    }, 200)
+})
