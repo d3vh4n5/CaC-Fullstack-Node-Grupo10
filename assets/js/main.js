df045838136cf13d26772c211c32d20a1c73a60f
Juan Basgall <juanangelbasgall@hotmail.com>
2024-05-11 23:43:49 -0300

    Reordenamiento de archivos js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import './components/index.js'
-import './modules/formsSendingData.js'
-import './modules/insideNav.mjs'
+import './utils/formsSendingData.js'
+import './utils/insideNav.mjs'
 
 
 console.info("%cscripts loaded", "color: green; background: lightgreen; padding: 5px 20px; border: 1px solid green;")
