c18c6be7f7b08b81c25dbf24391159e1516a2a5b
Marina Benitez <marina.benitez2611@gmail.com>
2024-05-21 18:31:29 -0300

    acomodado el login

---
+++ assets/js/utils/validateFunctions.js
@@ -0,0 +1,9 @@
+export const validateTextInput = ($input) =>{
+    console.log("estoy validado")
+}
+export const validateEmailInput = ($input) =>{
+    console.log("estoy validado email")
+}
+export const validatePasswordInput = ($input) =>{
+    console.log("estoy validado pass")
+}
