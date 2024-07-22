50740c1d5d897b238ee527085189c31d61d7893e
Juan Basgall <juanangelbasgall@hotmail.com>
2024-07-22 01:11:23 -0300

    Reparado error de envio de mensajes sin archivos

--- assets/js/modules/contact.js
+++ assets/js/modules/contact.js
@@ -12,10 +12,12 @@ validate($form, {
     typeExclude: ['file']
 })
 
+
 // Validando el input file
+let file = null
 $formFile.addEventListener('change', event => {
     // https://www.youtube.com/watch?v=nG2Rk01FB7k
-    const file = event.target.files[0]
+    file = event.target.files[0]
     const $formFileErrorsContainer = document.getElementById('formFileErrorsContainer')
 
     $formFileErrorsContainer.innerHTML = ''
@@ -40,64 +42,64 @@ $formFile.addEventListener('change', event => {
 
     if (!allowedTypes.includes(file.type)) {
         console.warn("Tipo de archivo no permitido")
-        $formFileErrorsContainer.innerText = `Tipo de archivo no permitido, por favor revise los tipos ed archivos habilitados.`;
+        $formFileErrorsContainer.innerText = `Tipo de archivo no permitido, por favor revise los tipos de archivos habilitados.`;
         $submitBtn.setAttribute('disabled', true)
         return
     }
 
     $submitBtn.removeAttribute('disabled')
 
-    // Controlando el submit del formulario
-    
-    $form.addEventListener('submit', async (event)=> {
-        event.preventDefault()
-        $submitBtn.setAttribute('disabled', true)
-        $submitBtn.innerHTML = LoadSpinner;
-    
-        // Tomo el valor que tiene el reCAPTCHA al momento del submit, 
-        // Este debería devolverme un token, si no lo tiene entonces
-        // El usuario no pasó la prueba.
-        const captcha = event.target[7].value
-    
-        console.log({ captcha })
-        // console.log(captcha !== '')
-        // console.log(captcha.length > 0 )
-        // Validacion de los inputs
-    
-        if (captcha !== ''){    
-            try {
-                const success = await submitFormData2(event)
-                
-                if (success) {
-                    Swal.fire({
-                        title: "¡Hecho!",
-                        text: "Hemos recibido su mensaje, gracias por contactarnos :)",
-                        icon: "success"
-                    });
-                    event.target.reset()
-                } else {
-                    throw new Error("No se pudo enviar el mensaje")
-                }
-            } catch (error) {
-                console.error(error)
+})
+
+// Controlando el submit del formulario
+
+$form.addEventListener('submit', async (event)=> {
+    event.preventDefault()
+    $submitBtn.setAttribute('disabled', true)
+    $submitBtn.innerHTML = LoadSpinner;
+
+    // Tomo el valor que tiene el reCAPTCHA al momento del submit, 
+    // Este debería devolverme un token, si no lo tiene entonces
+    // El usuario no pasó la prueba.
+    const captcha = event.target[7].value
+
+    console.log({ captcha })
+    // console.log(captcha !== '')
+    // console.log(captcha.length > 0 )
+    // Validacion de los inputs
+
+    if (captcha !== ''){    
+        try {
+            const success = await submitFormData2(event)
+            
+            if (success) {
                 Swal.fire({
-                    title: "Error",
-                    text: "Ocurrió un error al enviar el mensaje",
-                    icon: "error"
-                })
-            } finally {
-                $submitBtn.removeAttribute('disabled')
-                $submitBtn.innerHTML = "Enviar";
+                    title: "¡Hecho!",
+                    text: "Hemos recibido su mensaje, gracias por contactarnos :)",
+                    icon: "success"
+                });
+                event.target.reset()
+            } else {
+                throw new Error("No se pudo enviar el mensaje")
             }
-        } else {
+        } catch (error) {
+            console.error(error)
             Swal.fire({
                 title: "Error",
-                text: "Debe comprobar que no es un robot para continuar.",
+                text: "Ocurrió un error al enviar el mensaje",
                 icon: "error"
-            });
+            })
+        } finally {
             $submitBtn.removeAttribute('disabled')
             $submitBtn.innerHTML = "Enviar";
         }
-    })
+    } else {
+        Swal.fire({
+            title: "Error",
+            text: "Debe comprobar que no es un robot para continuar.",
+            icon: "error"
+        });
+        $submitBtn.removeAttribute('disabled')
+        $submitBtn.innerHTML = "Enviar";
+    }
 })
-
